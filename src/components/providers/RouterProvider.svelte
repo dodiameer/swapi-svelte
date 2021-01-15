f42a12d7c08e2e072cb3fab19268fff072c3490d
= <=>
2021-01-15 14:16:05 +0300

    Commented RouterProvider

--- src/components/providers/RouterProvider.svelte
+++ src/components/providers/RouterProvider.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
+  // Imports
   import AllPeople from "../routes/AllPeople.svelte";
   import PersonDetails from "../routes/PersonDetails.svelte";
   import Router from "svelte-spa-router";
+
+  // Route definitions
+  // Not separated into a TS file as it's only used here
   const routes = {
     "/": AllPeople,
     "/person/:id": PersonDetails,
   };
+
+  /* This is a provider for the Router component
+   * from svelte-spa-router.
+   * It's used inside the application component to
+   * turn the component into a layout component,
+   * where anything in the default slot is put above the route
+   * and anything in the footer slot is put below the route.
+   *
+   * I used this pattern in order to avoid wrapping every route in
+   * a layout component and instead use the App.svelte component
+   * to provide the global layout
+   */
 </script>
 
 <slot />
