f830538530d5d1bcca9d5794bc122d56d6961a33
= <=>
2021-01-15 14:24:27 +0300

    commented AllPeople route component

--- src/components/routes/AllPeople.svelte
+++ src/components/routes/AllPeople.svelte
@@ -1,5 +1,11 @@
 <script>
   import AllPeopleData from "../data/AllPeopleData.svelte";
+
+  /**
+   * This is the route component for AllPeopleData.
+   * It should contain no business logic except for
+   * UI purposes
+   */
 </script>
 
 <h1>This is a list of all the people available</h1>
