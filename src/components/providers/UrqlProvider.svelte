2830f64ae426622dfc1d3db85b328f93e2e5bdef
= <=>
2021-01-15 14:22:13 +0300

    Commented UrqlProvider

--- src/components/providers/UrqlProvider.svelte
+++ src/components/providers/UrqlProvider.svelte
@@ -3,6 +3,14 @@
   initClient({
     url: "https://dodi-swapi-gql.herokuapp.com/",
   });
+
+  /* This component abstracts away the initialization of URQL,
+   * and any children of the component are provided with the
+   * URQL client.
+   */
+
+  //! This should only be used once!
+  //TODO: refactor component to provide access to different APIs via URL prop
 </script>
 
 <slot />
