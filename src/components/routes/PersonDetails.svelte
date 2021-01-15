19c39493ea09eed99cde1c5865d5d4d5e6dceda2
= <=>
2021-01-15 14:25:49 +0300

    Commented person details route component

--- src/components/routes/PersonDetails.svelte
+++ src/components/routes/PersonDetails.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import PersonDetailsData from "../data/PersonDetailsData.svelte";
   export let params: { id: string } = { id: "" };
+
+  /**
+   * This is the route component for PersonDetailsData.
+   * It should contain no business logic except for
+   * UI purposes
+   */
 </script>
 
 <h1>Person details route</h1>
